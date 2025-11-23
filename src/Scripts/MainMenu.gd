eca73858b7adb61cac4f702aa184a0fc066a7fca
CerealKiller04 <kolumbanagnes04@gmail.com>
2025-11-23 22:39:59 +0200

    feature: main menu almost done+level selection

---
+++ src/Scripts/MainMenu.gd
@@ -0,0 +1,29 @@
+extends Node
+@onready var popUp= %Window
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_options_button_pressed() -> void:
+	print("stuff happens")
+	get_tree().change_scene_to_file("res://src/Scenes/Options.tscn")
+
+
+func _on_start_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/Scenes/LevelSelection.tscn")
+
+func _on_exit_button_pressed() -> void:
+	popUp.show()
+
+func _on_yes_button_pressed() -> void:
+	get_tree().quit() 
+	
+	
+func _on_no_button_pressed() -> void:
+	popUp.hide()
